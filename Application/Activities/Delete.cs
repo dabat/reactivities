578669c0cfbc9cae2d8b27de8470da093e65dd1d
daniel <daniel.battaglia@navitus.com>
2020-08-07 22:58:21 -0500

    initial draft of CRUD routes; added seed data

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                this._context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                {
+                    throw new Exception("activity was not found");
+                }
+
+                _context.Remove(activity);
+
+                return await _context.SaveChangesAsync() > 0
+                    ? Unit.Value
+                    : throw new Exception("Exception occurred while saving changes");
+            }
+        }
+    }
+}
